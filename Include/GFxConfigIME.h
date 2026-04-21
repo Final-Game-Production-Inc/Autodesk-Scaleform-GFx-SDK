daa386a1c5725b6d31f45feda8fe2793225ff5e2
FINALZ <79558453+FINALZ-27980@users.noreply.github.com>
2026-04-21 14:55:31 +0800

    Update GFxConfigIME.h

--- Include/GFxConfigIME.h
+++ Include/GFxConfigIME.h
@@ -1,7 +1,7 @@
 /**************************************************************************
 
 PublicHeader:   None
-Filename    :   GFxConfigAddons.h
+Filename    :   GFxConfigIME.h
 Content     :   GFx configuration file - contains #ifdefs for
                 the optional components of the library managed by
                 installers
@@ -9,21 +9,16 @@ Created     :
 Authors     :   
 
 Copyright   :   Copyright 2011 Autodesk, Inc. All Rights reserved.
+                Copyright 2026 Final Game Production Inc. All Rights reserved.
 
-Use of this software is subject to the terms of the Autodesk license
-agreement provided at the time of installation or download, or which
-otherwise accompanies this software in either electronic or hard copy form.
+Use of this software, PLEASE OBEY GPL-3.0 LICENSE.
 
 **************************************************************************/
-
-#define GFX_USE_VIDEO_WIN32
-#define GFX_USE_VIDEO_XBOX360
 
 #ifdef SF_OS_WIN32
 	#define SF_ENABLE_IME
 	#define SF_ENABLE_IME_WIN32
 #endif
-//#define SF_ENABLE_IME_XBOX360
 
 #if defined(SF_BUILD_DEBUG) || defined(SF_BUILD_DEBUGOPT)
     #define SF_FPE_ENABLE
